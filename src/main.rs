4437a109a1b8834b907a28fc2caf230cb4db2855
fabrice <fab.hatege15@gmail.com>
2023-01-16 14:40:22 +0100

    added some code/dependencies with sqlite

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,33 @@ mod importer;
 // ajout d'un module parseur
 
 use crate::parser::parse_query;
+use sqlite::Connection;
+
+fn get(connection: Connection) {
+    let query = "SELECT * FROM users WHERE age > 50";
+    connection
+        .iterate(query, |pairs| {
+            for &(name, value) in pairs.iter() {
+                println!("{} = {}", name, value.unwrap());
+            }
+            true
+        })
+        .unwrap();
+}
 
 fn main() {
     let res = parse_query("get $A $B $C such_as $A ami $B and $B == $C ");
+    println!("res: {:?}", res);
+
+    let connection = sqlite::open("data.db").unwrap();
+
+    let query = "
+        CREATE TABLE users (name TEXT, age INTEGER);
+        INSERT INTO users VALUES ('Alice', 42);
+        INSERT INTO users VALUES ('Bob', 69);
+    ";
+    connection.execute(query).unwrap();
+    let res = get(connection);
     println!("res: {:?}", res);
 }
 
