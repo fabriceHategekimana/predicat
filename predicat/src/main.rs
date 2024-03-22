f4ea76a4d1dafbd78eb786c8b1ffe46060f619f3
Fabrice <fab.hatege15@gmail.com>
2024-03-22 15:21:20 +0100

    Added main functionnalities into Interpreter

--- predicat/src/main.rs
+++ predicat/src/main.rs
@@ -13,45 +13,6 @@ use crate::parser::base_parser::PredicatAST;
 use crate::parser::parse_command;
 
 
-fn parse(command: &String) -> Vec<PredicatAST> {
-    parse_command(command).iter()
-                .map(PredicatAST::clone)
-                .flat_map(substitute_variables(SimpleContext::new()))
-                .flatten().collect()
-}
-
-fn get_user_passed_arguments() -> String {
-    env::args().skip(1)
-        .fold(String::new(), |acc, arg| format!("{}{} ", acc, &arg))
-}
-
-fn get_args_or(query: &str) -> String {
-    let args = get_user_passed_arguments();
-    if args == "".to_string() {
-        String::from(query)
-    }
-    else{
-        args
-    }
-}
-
-fn execute(cmds: &[PredicatAST], knowledge: &impl Knowledgeable) -> Option<SimpleContext> {
-    let context = knowledge
-            .valid_commands(cmds.to_vec())?
-            .iter()
-            .map(|x| knowledge.execute_command(x))
-            .reduce(SimpleContext::join_contexts)?;
-
-    Some(context.clone())
-}
-
-fn interpret(cmds: &[String], knowledge: &impl Knowledgeable) -> SimpleContext {
-    let cmds = cmds.iter()
-                .flat_map(parse)
-                .collect::<Vec<PredicatAST>>();
-    execute(&cmds, knowledge)
-                .expect("Something went wrong")
-}
 
 struct Interpreter {
     context: SimpleContext,
@@ -68,18 +29,58 @@ impl Interpreter {
     }
 
     fn run(&mut self) -> () {
-        let input_command = get_args_or("add socrate est mortel");
+        let input_command = self.get_args_or("add socrate est mortel");
 
         self.knowledge.clear_cache();
 
-        let mut context = interpret(&vec![input_command], &self.knowledge);
+        let mut context = self.interpret(&vec![input_command], &self.knowledge);
 
         while context.has_commands() && !context.has_error() {
-            context = interpret(&context.get_aftercmds(), &self.knowledge);
+            context = self.interpret(&context.get_aftercmds(), &self.knowledge);
         }
 
         context.display(); //display context or error
         self.context = context;
+    }
+
+    fn parse(command: &String) -> Vec<PredicatAST> {
+        parse_command(command).iter()
+                    .map(PredicatAST::clone)
+                    .flat_map(substitute_variables(SimpleContext::new()))
+                    .flatten().collect()
+    }
+
+    fn get_user_passed_arguments(&self) -> String {
+        env::args().skip(1)
+            .fold(String::new(), |acc, arg| format!("{}{} ", acc, &arg))
+    }
+
+    fn get_args_or(&self, query: &str) -> String {
+        let args = self.get_user_passed_arguments();
+        if args == "".to_string() {
+            String::from(query)
+        }
+        else{
+            args
+        }
+    }
+
+    fn execute(&self, cmds: &[PredicatAST], knowledge: &impl Knowledgeable) -> Option<SimpleContext> {
+        let context = knowledge
+                .valid_commands(cmds.to_vec())?
+                .iter()
+                .map(|x| knowledge.execute_command(x))
+                .reduce(SimpleContext::join_contexts)?;
+
+        Some(context.clone())
+    }
+
+    fn interpret(&self, cmds: &[String], knowledge: &impl Knowledgeable) -> SimpleContext {
+        let cmds = cmds.iter()
+                    .flat_map(Self::parse)
+                    .collect::<Vec<PredicatAST>>();
+        self.execute(&cmds, knowledge)
+                    .expect("Something went wrong")
     }
 }
 
@@ -94,4 +95,5 @@ impl Default for Interpreter {
 
 fn main() {
         //let knowledge = new_knowledge("sqlite").expect("Can't open the knowledge!");
+    let interpreter = Interpreter::default();
 }
